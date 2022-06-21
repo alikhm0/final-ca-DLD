bb969bbc152568e7509af7270d57abfe9f0d9f43
alikhm0 <87974441+alikhm0@users.noreply.github.com>
2022-06-21 19:08:43 +0430

    Update LUTExp.v

--- final-ca-DLD-verilog file/LUTExp.v
+++ final-ca-DLD-verilog file/LUTExp.v
@@ -3,19 +3,22 @@ reg [15:0] datat;
 	always @(adr) begin
 		case(adr)
 			0: datat = 16'hFFFF;
-			1: datat = 16'h8000;//1/2
-			2: datat = 16'h5555;//1/3
-			3: datat = 16'h4000;// 1/4
-			4: datat = 16'h3333;// 1/5
-			5: datat = 16'h2AAA;// 1/6
-			6: datat = 16'h2492;// 1/7
-			7: datat = 16'h2000;// 1/8
+			1: datat = 16'h8000;
+			2: datat = 16'h5555;
+			3: datat = 16'h4000;
+			4: datat = 16'h3333;
+			5: datat = 16'h2AAA;
+			6: datat = 16'h2492;
+			7: datat = 16'h2000;
+			8: datat = 16'h1C71;
+			9: datat = 16'h1999;
+			10: datat = 16'h1745;
+			11: datat = 16'h1555;
+			12: datat = 16'h13B1;
+			13: datat = 16'h1249;
+			14: datat = 16'h1111;
+			15: datat = 16'h1000;
 		endcase
 	end
 assign data = datat;
 endmodule
-
-
-
-
-
